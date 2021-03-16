75505fc782b27ad21609500a24237bac35f7eedb
Joao <silva.joao1@outlook.com.br>
2021-03-16 12:28:43 -0300

    Send email jobs exception to Sentry

--- src/app/jobs/CanceledAppointmentMail.js
+++ src/app/jobs/CanceledAppointmentMail.js
@@ -1,27 +1,33 @@
 import { format, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Sentry from '@sentry/node';
+
 import Mail from '../../lib/Mail';
 
 const CanceledAppointmentMail = {
   key: 'CanceledAppointmentMail',
 
   handle: async ({ data }) => {
-    const { appointment } = data;
-    await Mail.sendMail({
-      to: `${appointment.user.name} <${appointment.user.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'canceledAppointment',
-      context: {
-        user: appointment.user.name,
-        date: format(
-          parseISO(appointment.date),
-          "'dia' dd 'de' MMMM', às' HH:mm'h'",
-          {
-            locale: pt,
-          }
-        ),
-      },
-    });
+    try {
+      const { appointment } = data;
+      await Mail.sendMail({
+        to: `${appointment.user.name} <${appointment.user.email}>`,
+        subject: 'Agendamento cancelado',
+        template: 'canceledAppointment',
+        context: {
+          user: appointment.user.name,
+          date: format(
+            parseISO(appointment.date),
+            "'dia' dd 'de' MMMM', às' HH:mm'h'",
+            {
+              locale: pt,
+            }
+          ),
+        },
+      });
+    } catch (err) {
+      Sentry.captureException(err);
+    }
   },
 };
 
--- src/app/jobs/SuccessAppointmentMail.js
+++ src/app/jobs/SuccessAppointmentMail.js
@@ -1,23 +1,29 @@
 import { format, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Sentry from '@sentry/node';
+
 import Mail from '../../lib/Mail';
 
 const SuccessAppointmentMail = {
   key: 'SuccessAppointmentMail',
 
   handle: async ({ data }) => {
-    const { user, date } = data;
-    await Mail.sendMail({
-      to: `${user.name} <${user.email}>`,
-      subject: 'Agendamento concluído',
-      template: 'successAppointment',
-      context: {
-        user: user.name,
-        date: format(parseISO(date), "'dia' dd 'de' MMMM', às' HH:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
+    try {
+      const { user, date } = data;
+      await Mail.sendMail({
+        to: `${user.name} <${user.email}>`,
+        subject: 'Agendamento concluído',
+        template: 'successAppointment',
+        context: {
+          user: user.name,
+          date: format(parseISO(date), "'dia' dd 'de' MMMM', às' HH:mm'h'", {
+            locale: pt,
+          }),
+        },
+      });
+    } catch (err) {
+      Sentry.captureException(err);
+    }
   },
 };
 
